165eb272abc694ac333b53b8ee03f76619918f33
jmilillo <jmilillo@newforma.com>
2022-02-03 18:26:37 -0500

    removed image

--- sayno.py
+++ sayno.py
@@ -60,7 +60,7 @@ def get_middle_html(word_dict):
             <tr>
                 <td class="english"><span>''', english, '''</span></td>
                 <td class="phonetic">
-                    <span class="leftPad">''', phonetic, ''' <img src="images/play.jpeg" class="play" />
+                    <span class="leftPad">''', phonetic, '''
                     <audio id="''', file_name, '''" src="./sounds/''', file_name, '''" controls/>
                     </span>
                 </td>
