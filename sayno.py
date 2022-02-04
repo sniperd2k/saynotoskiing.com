8f0914d5731993f34164b406ada72bd11537a5e5
jmilillo <jmilillo@newforma.com>
2022-02-04 08:07:38 -0500

    Moved click to td

--- sayno.py
+++ sayno.py
@@ -49,12 +49,12 @@ def get_middle_html(word_dict):
             '''
             <tr>
                 <td class="english"><span>''', english, '''</span></td>
-                <td class="phonetic">
+                <td class="phonetic" onclick="toggleAudio(\'''',file_name,'''\')">
                     <span class="leftPad">''', phonetic, '''</span>
                     <div class="audio-player">
                         <div class="controls">
                             <audio id="''', file_name, '''" src="./sounds/''', file_name, '''" onended="audioEnded(\'''',button_id,'''\')"></audio>
-                            <button id="''',button_id,'''" class="player-button" onclick="toggleAudio(\'''',file_name,'''\')">
+                            <button id="''',button_id,'''" class="player-button">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#3D3132">
                                     <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd" />
                                 </svg>
