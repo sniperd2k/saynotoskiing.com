1c9db540111ec9084e62af9390b16c435d6c83ed
David Underwood <djhertz@gmail.com>
2022-02-19 19:16:42 -0500

    added need/want

--- sayno.py
+++ sayno.py
@@ -22,6 +22,7 @@ def get_word_dict():
     "Look":"kahn",
     "Me":"wha",
     "Not good":"bu how",
+    "Need/Want":"yao",
     "Patience":"neigh xeen",
     "Right side":"yo bien",
     "Ski":"hwah sooyeh",
